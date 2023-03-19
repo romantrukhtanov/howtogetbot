70f5c12cd06b0398f71f04ff30fd467d207998f3
romantrukhtanov <romantrukhtanov@gmail.com>
2023-03-19 09:19:54 +0500

    updated: project dependencies

--- src/services/aws/index.ts
+++ src/services/aws/index.ts
@@ -1,49 +1,42 @@
-import AWS from 'aws-sdk';
+import { PutObjectCommandInput, S3 } from '@aws-sdk/client-s3';
 import { v4 } from 'uuid';
 
 import {
   AWS_REGION,
+  S3_BUCKET_NAME,
   AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY,
-  S3_BUCKET_NAME,
 } from 'shared/helpers/env';
 
 class Aws {
   constructor() {
-    this.updateConfig();
-    this.S3 = new AWS.S3({
-      accessKeyId: AWS_ACCESS_KEY_ID,
-      secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    this.S3 = new S3({
+      region: AWS_REGION,
+      credentials: {
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY,
+      },
     });
   }
 
-  private readonly S3: AWS.S3;
+  private readonly S3: S3;
 
-  public async getObject(key: AWS.S3.ObjectKey, bucket: AWS.S3.BucketName = S3_BUCKET_NAME) {
-    const response = this.S3.getObject({
+  public async getObject(key: string, bucket: string = S3_BUCKET_NAME) {
+    return this.S3.getObject({
       Bucket: bucket,
       Key: key,
     });
-
-    return response.createReadStream();
   }
 
   public async putObject(
-    body: AWS.S3.Body,
+    body: PutObjectCommandInput['Body'],
     ext: string,
-    bucket: AWS.S3.BucketName = S3_BUCKET_NAME,
+    bucket: string = S3_BUCKET_NAME,
   ) {
-    const key = v4();
     return this.S3.putObject({
       Body: body,
-      Key: key + ext,
+      Key: v4() + ext,
       Bucket: bucket,
-    }).promise();
-  }
-
-  private updateConfig() {
-    AWS.config.update({
-      region: AWS_REGION,
     });
   }
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,21 +1,21 @@
 import { HttpActions } from 'services/httpActions';
 import { Download } from 'services/download';
-import { Aws } from 'services/aws';
 import { MapsApi } from 'services/mapsApi';
+// import { Aws } from 'services/aws';
 import type { RootController } from 'core/rootController';
 
 class Services {
   constructor(public rootController: RootController) {
     this.httpActions = new HttpActions();
     this.download = new Download();
-    this.aws = new Aws();
     this.mapsApi = new MapsApi();
+    // this.aws = new Aws();
   }
 
   readonly httpActions: HttpActions;
   readonly download: Download;
-  readonly aws: Aws;
   readonly mapsApi: MapsApi;
+  // readonly aws: Aws;
 }
 
 export { Services };
